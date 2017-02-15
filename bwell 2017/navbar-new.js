3f97345b8d1137bd1218b57bd57de2af4afdc6df
Thomas Moran @OSX <thomas@optimizme.com>
2017-02-15 17:00:00 +0000

    updating and re-org of files

---
+++ bwell 2017/navbar-new.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+    $(".content-panel").load("navbar-home.html");
+});
+
+
+/*$("ul.navbar-nav li").each(function() {
+    $(this).on("click", function () {
+        $(".content-panel").load(($this).attr("data-page")+".html");
+    });
+});*/
+
+
+ 
+//  $('li').on('click', function() {
+
+$(document).find( )
+    
+$("ul.navbar-nav li").each(function() {
+    //$(this).hide();
+    alert('in here!');
+    if($(this).is('.special')) {
+        alert("special");
+    }
+
+});  
+
+            
+//});
