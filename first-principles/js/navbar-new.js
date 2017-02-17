b615e4e2875a923ffc04af990d127ce2b889fb48
Thomas Moran @OSX <thomas@optimizme.com>
2017-02-17 14:03:44 +0000

    start of integration into main site

--- first-principles/js/navbar-new.js
+++ first-principles/js/navbar-new.js
@@ -1,28 +1,12 @@
-$(document).ready(function() {
-    $(".content-panel").load("navbar-home.html");
-});
+$(document).ready(function () {
+    $(".jumbotron").load("navbar-home.html");
 
-
-/*$("ul.navbar-nav li").each(function() {
-    $(this).on("click", function () {
-        $(".content-panel").load(($this).attr("data-page")+".html");
+    $("ul.navbar-nav li").each(function () {
+        $(this).on('click', function () {
+            var elem = $(this);
+            console.log(elem[0].innerHTML);
+            console.log($("#contactlink"));
+            $(".jumbotron").load(elem.attr('data-page')+".html");
+        });
     });
-});*/
-
-
- 
-//  $('li').on('click', function() {
-
-$(document).find( )
-    
-$("ul.navbar-nav li").each(function() {
-    //$(this).hide();
-    alert('in here!');
-    if($(this).is('.special')) {
-        alert("special");
-    }
-
-});  
-
-            
-//});
+});
